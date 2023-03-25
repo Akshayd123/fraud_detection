99ad923b29e75e063480b4236eb2665b15648543
Akshayd123 <akshaydharma56@gmail.com>
2023-03-25 08:40:44 -0400

    added and changed ml model code

---
+++ model_ml.py
@@ -0,0 +1,8 @@
+print("adding allML related code here")
+
+def model1():
+    print("Adding code for ML Model1")
+def model2():
+    print("Adding code for ML Model2")
+def model3():
+    print("Adding code for ML Model3")
